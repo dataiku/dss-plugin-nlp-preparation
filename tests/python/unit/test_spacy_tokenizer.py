f54fd756a8f410caec42939dd3bf31f0eb833892
alexcombessie <alex.combessie@gmail.com>
2020-09-09 13:30:40 +0200

    working unit-test for tokenizer

--- tests/python/unit/test_spacy_tokenizer.py
+++ tests/python/unit/test_spacy_tokenizer.py
@@ -3,8 +3,32 @@
 # pytest automatically runs all the function starting with "test_"
 # see https://docs.pytest.org for more information
 
+import pandas as pd
+
 from spacy_tokenizer import MultilingualTokenizer  # noqa
 
 
-def test_spellcheck_df():
-    assert True
+def test_tokenize_df_english():
+    input_df = pd.DataFrame({"input_text": ["I hope nothing. I fear nothing. I am free. 💩 😂 #OMG"],})
+    tokenizer = MultilingualTokenizer()
+    output_df = tokenizer.tokenize_df(df=input_df, text_column="input_text", language="en")
+    tokenized_document = output_df[tokenizer.tokenized_column][0]
+    assert len(tokenized_document) == 15
+
+
+def test_tokenize_df_multilingual():
+    input_df = pd.DataFrame(
+        {
+            "input_text": [
+                "I hope nothing. I fear nothing. I am free.",
+                " Les sanglots longs des violons d'automne",
+                "子曰：“學而不思則罔，思而不學則殆。”",
+            ],
+            "language": ["en", "fr", "zh"],
+        }
+    )
+    tokenizer = MultilingualTokenizer()
+    output_df = tokenizer.tokenize_df(df=input_df, text_column="input_text", language_column="language")
+    tokenized_documents = output_df[tokenizer.tokenized_column]
+    tokenized_documents_length = [len(doc) for doc in tokenized_documents]
+    assert tokenized_documents_length == [12, 8, 13]
--- tests/python/unit/test_symspell_checker.py
+++ tests/python/unit/test_symspell_checker.py
@@ -3,8 +3,33 @@
 # pytest automatically runs all the function starting with "test_"
 # see https://docs.pytest.org for more information
 
+import pandas as pd
+
 from symspell_checker import SpellChecker  # noqa
 
 
-def test_spellcheck_df():
+def test_spellcheck_df_english():
+    input_df = pd.DataFrame(
+        {
+            "input_text": [
+                "Can yu readtHIS messa ge despite thehorible AB1234  sppelingmsitakes",
+                "Whereis th elove hehaD Dated forImuch of thepast who couqdn'tread in sixthgrade AND ins pired him",
+                "!? Nooo way I CAN'T haz cheezburger 💩 😂 #OMG",
+            ],
+        }
+    ).sort_values(by=["input_text"])
+    assert True
+
+
+def test_spellcheck_df_multilingual():
+    input_df = pd.DataFrame(
+        {
+            "input_text": [
+                "Can yu readtHIS messa ge despite thehorible AB1234  sppelingmsitakes",
+                "Les fautes d'ortograf c pas toop #LOOOL PTDR",
+                "covfefe tnetennba rosebud",
+            ],
+            "language": ["en", "fr", "unknown"],
+        }
+    ).sort_values(by=["input_text"])
     assert True
