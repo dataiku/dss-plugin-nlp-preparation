eb5c9a01a629aa5d899d42081813efaa1cc099d5
Alex Combessie <alex.combessie@gmail.com>
2020-10-27 19:11:00 +0100

    Remove None from language scope UI parameter

--- resource/get_language_list.py
+++ resource/get_language_list.py
@@ -12,5 +12,6 @@ def do(payload, config, plugin_config, inputs):
         language_choices = sorted(
             [{"value": k, "label": v} for k, v in SUPPORTED_LANGUAGES_PYCLD3.items()], key=lambda x: x.get("label")
         )
-        language_choices.insert(0, {"label": "None", "value": "None"})
+        if payload["parameterName"] == "fallback_language":
+            language_choices.insert(0, {"label": "None", "value": "None"})
     return {"choices": language_choices}
