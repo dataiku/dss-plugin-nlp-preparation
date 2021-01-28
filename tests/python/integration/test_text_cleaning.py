ef434fa1e82b706e6943ed059bf952c456064d78
Alex Combessie <alex.combessie@gmail.com>
2021-01-28 16:02:23 +0100

    Update test_text_cleaning.py

--- tests/python/integration/test_text_cleaning.py
+++ tests/python/integration/test_text_cleaning.py
@@ -1,28 +1,14 @@
 # -*- coding: utf-8 -*-
-import pytest
-import logging
-
 from dku_plugin_test_utils import dss_scenario
 
-pytestmark = pytest.mark.usefixtures("plugin", "dss_target")
 
-test_kwargs = {
-    "user": "user1",
-    "project_key": "TEST_TEXTPREPARATIONPLUGIN",
-    "logger": logging.getLogger("dss-plugin-test.nlp-preparation.test_text_cleaning"),
-}
+def test_textcleaning_english_tweets(user_dss_clients):
+    dss_scenario.run(user_dss_clients, project_key="TEST_TEXTPREPARATIONPLUGIN", scenario_id="TEST_TEXTCLEANING_TWEETS")
 
 
-def test_textcleaning_english_tweets(user_clients):
-    test_kwargs["client"] = user_clients[test_kwargs["user"]]
-    dss_scenario.run(scenario_id="TEST_TEXTCLEANING_TWEETS", **test_kwargs)
+def test_textcleaning_multilingual_news(user_dss_clients):
+    dss_scenario.run(user_dss_clients, project_key="TEST_TEXTPREPARATIONPLUGIN", scenario_id="TEST_TEXTCLEANING_NEWS")
 
 
-def test_textcleaning_multilingual_news(user_clients):
-    test_kwargs["client"] = user_clients[test_kwargs["user"]]
-    dss_scenario.run(scenario_id="TEST_TEXTCLEANING_NEWS", **test_kwargs)
-
-
-def test_textcleaning_edge_cases(user_clients):
-    test_kwargs["client"] = user_clients[test_kwargs["user"]]
-    dss_scenario.run(scenario_id="TEST_TEXTCLEANING_EDGECASES", **test_kwargs)
+def test_textcleaning_edge_cases(user_dss_clients):
+    dss_scenario.run(user_dss_clients, project_key="TEST_TEXTPREPARATIONPLUGIN", scenario_id="TEST_TEXTCLEANING_EDGECASES")
