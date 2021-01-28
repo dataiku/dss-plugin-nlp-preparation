fd5ef9d5ddf2c1d1a038c9a94f52d85c71f047b9
Alex Combessie <alex.combessie@gmail.com>
2021-01-28 15:59:58 +0100

    Update test_language_detection.py

--- tests/python/integration/test_language_detection.py
+++ tests/python/integration/test_language_detection.py
@@ -1,18 +1,6 @@
 # -*- coding: utf-8 -*-
-import pytest
-import logging
-
 from dku_plugin_test_utils import dss_scenario
 
-pytestmark = pytest.mark.usefixtures("plugin", "dss_target")
 
-test_kwargs = {
-    "user": "user1",
-    "project_key": "TEST_TEXTPREPARATIONPLUGIN",
-    "logger": logging.getLogger("dss-plugin-test.nlp-preparation.test_language_detection"),
-}
-
-
-def test_langdetect_wili_benchmark(user_clients):
-    test_kwargs["client"] = user_clients[test_kwargs["user"]]
-    dss_scenario.run(scenario_id="TESTLANGUAGEDETECTIONONWILI", **test_kwargs)
+def test_langdetect_wili_benchmark(user_dss_clients):
+    dss_scenario.run(user_dss_clients, project_key="TEST_TEXTPREPARATIONPLUGIN", scenario_id="TESTLANGUAGEDETECTIONONWILI")
