68ade13d55c3ba4beeed82418bc52b250fa3169b
Alex Combessie <alex.combessie@gmail.com>
2021-04-20 14:30:26 +0200

    Update get_spellchecking_params.py

--- resource/get_spellchecking_params.py
+++ resource/get_spellchecking_params.py
@@ -7,5 +7,5 @@ def do(payload, config, plugin_config, inputs):
         [{"value": k, "label": v} for k, v in SUPPORTED_LANGUAGES_SYMSPELL.items() if k in SUPPORTED_LANGUAGES_SPACY],
         key=lambda x: x.get("label"),
     )
-    language_choices.insert(0, {"label": "Detected language column", "value": "language_column"})
+    language_choices.insert(0, {"label": "Multilingual", "value": "language_column"})
     return {"choices": language_choices}
